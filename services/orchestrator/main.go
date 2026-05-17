23ae1c2f41be457ef0bc4c8aac66be826ec182c5
Sign0ret <adolfo.h.signoret@gmail.com>
2026-05-17 16:17:58 -0600

    fix: allow SSH in firewall, update /ping for deploy test

--- services/orchestrator/main.go
+++ services/orchestrator/main.go
@@ -22,7 +22,7 @@ func main() {
 
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(map[string]string{"pong": "true"})
+		json.NewEncoder(w).Encode(map[string]string{"pong": "verified"})
 	})
 
 	log.Printf("orchestrator listening on :%s", port)
